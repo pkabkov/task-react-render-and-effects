84059e7c1e707b6fde3cf5721b6748d7c275271e
Peter <petyakabkov@gmail.com>
2025-05-27 15:24:11 +0300

    Сделано

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,5 +1,25 @@
+import React, { useEffect, useState, useCallback } from 'react';
 import { subscribe, unsubscribe } from './resources/API';
 
 export function Effects(props: { sourceId: string }) {
-    return <div>123</div>;
+    const { sourceId } = props;
+    const [lastMessage, setLastMessage] = useState<string | number>('-1');
+
+    const onMessage = useCallback((message: number) => {
+        setLastMessage(message);
+    }, []);
+
+    useEffect(() => {
+        setLastMessage('-1');
+        subscribe(sourceId, onMessage);
+        return () => {
+            unsubscribe(sourceId, onMessage);
+        };
+    }, [sourceId, onMessage]);
+
+    return (
+        <div>
+            {sourceId}: {lastMessage}
+        </div>
+    );
 }
